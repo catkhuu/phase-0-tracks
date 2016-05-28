72124b57465191b1fde1970c4fa850b6c71c12e1
catkhuu <sunshine.dumbo@gmail.com>
2016-05-28 10:03:07 -0400

    added intake questions for hamster adoption. Set estimated age variable to nil.

---
+++ ruby/hamsters.rb
@@ -0,0 +1,21 @@
+puts "Hamster's name?"
+name = gets.chomp
+puts "Volume level from 1 to 10?"
+volume = gets.chomp
+puts "Fur color?"
+fur_color = gets.chomp
+puts "Good candidate for adoption?"
+adoption = gets.chomp
+puts "Estimated age?"
+age = nil
+age ||= gets.chomp
+
+
+
+
+
+
+
+
+
+
