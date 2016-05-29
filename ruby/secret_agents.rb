e079c9fe883774a76cd8d15d6591e8761c68b281
catkhuu <sunshine.dumbo@gmail.com>
2016-05-29 17:32:26 -0400

    turned encrypter and decrypter into methods

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -7,19 +7,22 @@
 #- Combine the proceeding characters
 #- Print the combined proceeding characters
 
-puts "Please enter your password"
-  password = gets.chomp.downcase
+puts "Enter your password"
+password = gets.chomp.downcase
 
-index = 0
+def encrypter(password)
+  index = 0
 
-while index < password.length
-  password[index] = password[index].next
-  index +=1
+  while index < password.length
+    password[index] = password[index].next!
+    index +=1
+    return password
+  end
 end
 
-puts password
+puts encrypter(password)
 
-##Decrypt
+  ##Decrypt
 # Identify the password produced by the encryption tool
 # Initiate a loop
 # Introduce the password into the loop
@@ -31,23 +34,25 @@ puts password
 puts "Enter the encrypted password"
   encrypted_password = gets.chomp.downcase
 
-index = 0
-while index < encrypted_password.length
-  str = "abcdefghijklmnopqrstuvwxyz"
-  current_letter = encrypted_password[index]
+def decrypter(encrypted_password)
+  index = 0
+
+  while index < encrypted_password.length
+    str = "abcdefghijklmnopqrstuvwxyz"
+    current_letter = encrypted_password[index]
+
   #For password of "hello", expect first current_letter to be "h"
   #Find the index of current letter in string. And store in variable current_letter_position
   #Reduce current_letter_position by 1. Store in variable decrypted_letter_position
   #Find the letter that corresponds to decrypted_letter_position from str variable. Store in variable decrypted_letter
   #Replace characters in encrypted password with decrypted_letter.
-  current_letter_position = str.index(current_letter)
-  decrypted_letter_position = current_letter_position-1
-  decrypted_letter = str[decrypted_letter_position]
-  encrypted_password[index] = decrypted_letter
-  index +=1
+    current_letter_position = str.index(current_letter)
+    decrypted_letter_position = current_letter_position-1
+    decrypted_letter = str[decrypted_letter_position]
+    encrypted_password[index] = decrypted_letter
+    index +=1
+    return encrypted_password
+  end
 end
 
-puts encrypted_password
-
-
-
+puts decrypter(encrypted_password)
