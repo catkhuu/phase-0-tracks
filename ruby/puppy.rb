765f1f8db9e9344dbb941ab63ba5f6b36e12cbd3
catkhuu <sunshine.dumbo@gmail.com>
2016-06-07 17:36:51 -0400

    added characteristics and behavior for corgbeagie greyrusshound species

---
+++ ruby/puppy.rb
@@ -0,0 +1,23 @@
+## Puppy Class (this feels so unnatural to create the ultimate corgi, just sayin'...)
+
+# SPECIES
+# Corbeagie Greyrusshound
+
+# CHARACTERISTICS
+# size: medium
+# coat: medium
+# color: white, brown, and black
+# friendly: yes
+# ears: pointed
+# eyes: human-like
+# stature: varies, but mostly lean
+# leg length: short
+
+# BEHAVIOR
+# active
+# herd anything that moves and breathes
+# snuggler
+# food motivated
+# responds well to being petted
+# wimpers when picked up
+# sniffs out peanut butter pretzels
