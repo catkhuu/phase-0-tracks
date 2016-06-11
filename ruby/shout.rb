8bf141c78dcdfe3d6376db51be3d27d8f2b26684
catkhuu <sunshine.dumbo@gmail.com>
2016-06-11 13:40:27 -0400

    added yelling_happily method to shout module. Added driver code for both yell_angrily method and yelling_happily method.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,5 +1,16 @@
+# Define and declare module
+
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
   end
+
+  def self.yelling_happily(words)
+    words.upcase + "!!!" + ":D :D :D :D :D"
+  end
 end
+
+# Call module methods
+
+Shout.yell_angrily("Not the ice cream")
+Shout.yelling_happily("we won")
