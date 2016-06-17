39fdaa95489eeeb77fd5b09634f3e02e5ccd4cc3
catkhuu <sunshine.dumbo@gmail.com>
2016-06-17 07:44:47 -0400

    pseudocoded reverse(str) function. Included condional statement to reverse inputted value only if value is a string. Else, print error message if inputted value is not a string.

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// reverse string
+// input: string
+// output: reverse string
+// IF var str is a string (both in type and value) OR as an instance of a string, then print reverse string.
+//  REVERSE STRING:
+//   - take string and convert to array using .split
+//   - reverse array using .reverse
+//   - convert array back into a string using .join
+// ELSE if var str is not a string, then return "Error: inputted value not a string. Please input a string. (Ex. 'hello')"
