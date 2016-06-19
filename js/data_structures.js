76bae45666c08a7ba69f912882ebd56c3c8f4453
catkhuu <sunshine.dumbo@gmail.com>
2016-06-19 14:27:43 -0400

    pseudocoded horseDirectory function

--- js/data_structures.js
+++ js/data_structures.js
@@ -13,5 +13,13 @@ horseNames.push(name);
 console.log(horseNames);
 }
 
+// loop through colors and horseNames arrays while i is less than colors.length. Pairing of key-values for object(hash) will only work if there are the quantity in each array are equal.
+// create new object - var horseId
+// set loop
+// to add a new item key-value pair to object, it's objectName[key] = value. In this case, we want our key to be horseNames and its value to be colors.
+// access horseNames and colors in array by using .slice(begin, end). begin = i, while end = (i+1). To pair 'Ranger' with 'royal blue' it would be horseId[horseNames.slice(1,(1+2))] = colors.slice(1,(1+2));
+// print object horseId
+// problem with this method is it will return slice for each key-value pair until i = colors.length. It won't just return the final object with all 5 key-value pairs.
+
 addColor("neon grey");
 addHorseName("Chancie");
