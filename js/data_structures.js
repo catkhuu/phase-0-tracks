6e567236ef032b39ab696b3870696e008433f3de
catkhuu <sunshine.dumbo@gmail.com>
2016-06-19 11:51:02 -0400

    declared two arrays - colors and horseNames

---
+++ js/data_structures.js
@@ -0,0 +1,3 @@
+// declare two arrays. One for colors and the other for horse names.
+var colors = ["rainbow", "royal blue", "purple rain", "gold sparkles"];
+var horseNames = ["Jack", "Ranger", "Chuck", "Cheerio"];
