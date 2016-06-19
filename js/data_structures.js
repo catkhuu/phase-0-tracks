12f70a1a985d635c8a055360cc72482dec6d5734
catkhuu <sunshine.dumbo@gmail.com>
2016-06-19 11:52:17 -0400

    added two functions - addColor and addHorseName to add additional color and horse name to their respective arrays. Driver code to test functions.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,17 @@
 // declare two arrays. One for colors and the other for horse names.
 var colors = ["rainbow", "royal blue", "purple rain", "gold sparkles"];
 var horseNames = ["Jack", "Ranger", "Chuck", "Cheerio"];
+// add additional color to colors array
+function addColor(color) {
+colors.push(color);
+console.log(colors);
+}
+
+// add additional horse name to horseNames array
+function addHorseName(name) {
+horseNames.push(name);
+console.log(horseNames);
+}
+
+addColor("neon grey");
+addHorseName("Chancie");
