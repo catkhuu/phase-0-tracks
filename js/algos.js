7320134ef86e4725cb2e890278df779d9261f306
catkhuu <sunshine.dumbo@gmail.com>
2016-06-19 21:20:02 -0400

    pseudocode for longestStr function

---
+++ js/algos.js
@@ -0,0 +1,4 @@
+// input: array of strings
+// output: longest string
+// take given arrayOfStrings and use sort function on Array. arrayOfStrings will be sorted from descending order, with the longest string as index = 0.  Store sorted array in variable sorted.
+// print sorted[0] with console.log. Should return the longest string in the given array.
